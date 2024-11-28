2e6741b988fd331b615e5f7ccc5f483984ae570c
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2024-11-28 19:03:38 +0600

    26. Remove Duplicates from Sorted Array

--- Two Pointer/26. Remove Duplicates from Sorted Array.cpp
+++ Two Pointer/26. Remove Duplicates from Sorted Array.cpp
@@ -11,3 +11,19 @@ public:
         return j;
     }
 };
+/*_---------------------------C++-----------------------------_*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = (int)nums.size();
+        int index = 1;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] != nums[index - 1]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        nums.resize(index);
+        return index;
+    }
+};
