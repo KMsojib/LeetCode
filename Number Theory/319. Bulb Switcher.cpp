325425d60c3f52a031a218379788da9fb5ddacf1
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2024-12-02 23:28:31 +0600

    319. Bulb Switcher

---
+++ Number Theory/319. Bulb Switcher.cpp
@@ -0,0 +1,10 @@
+// # Problem          : 319. Bulb Switcher
+// # Time Complexity  : 
+// # C++ Solution
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
