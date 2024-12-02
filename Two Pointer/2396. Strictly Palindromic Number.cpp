952f7414cd799ddd6d65d1e807ec3523db6d01a6
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2024-12-02 22:56:00 +0600

    2396. Strictly Palindromic Number

---
+++ Two Pointer/2396. Strictly Palindromic Number.cpp
@@ -0,0 +1,11 @@
+/*
+# Problem          : 2396. Strictly Palindromic Number
+# Time Complexity  : O(1)
+# C++ Solution
+*/
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
