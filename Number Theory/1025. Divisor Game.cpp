08e668bd29fa53de9c611d41a5242eb7de08909a
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2024-12-02 23:00:28 +0600

    1025. Divisor Game

---
+++ Number Theory/1025. Divisor Game.cpp
@@ -0,0 +1,12 @@
+/*
+# Problem          : 1025. Divisor Game
+# Time Complexity  : O(1)
+# C++ Solution
+
+*/
+class Solution {
+public:
+    bool divisorGame(int n) {
+        return n%2 == 0 ? true : false;
+    }
+};
