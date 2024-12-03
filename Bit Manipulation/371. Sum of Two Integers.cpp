7b23957e5c3ef5b93807e21af1a001e93664e3f8
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2024-12-03 23:27:27 +0600

    371. Sum of Two Integers

---
+++ Bit Manipulation/371. Sum of Two Integers.cpp
@@ -0,0 +1,16 @@
+/*
+# Problem          : 371. Sum of Two Integers
+# Time Complexity  : 
+# C++ Solution
+*/
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a ^= b;
+            b = carry;
+        }
+        return a;
+    }
+};
