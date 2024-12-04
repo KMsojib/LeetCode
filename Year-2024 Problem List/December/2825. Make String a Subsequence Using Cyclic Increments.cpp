0e0e7e46a3b58f325cec250d50e339905f9a4bc6
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2024-12-04 09:22:48 +0600

    2825. Make String a Subsequence Using Cyclic Increments

---
+++ Year-2024 Problem List/December/2825. Make String a Subsequence Using Cyclic Increments.cpp
@@ -0,0 +1,28 @@
+/*
+# Problem          : 2825. Make String a Subsequence Using Cyclic Increments
+# Time Complexity  : O(n+m)
+# C++ Solution
+Time: 27 minute
+*/
+/*---------------------------------*Two Pointer*---------------------------------*/
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int n = (int)str1.size();
+        int m = (int)str2.size();
+        bool ok = true;
+        int l = 0, r = 0;
+        while (l < n && r < m) {
+            if (str1[l] == str2[r]) {
+                l++, r++;
+            } else if ((str1[l] + 1) == str2[r]) {
+                l++, r++;
+            } else if ((str1[l] == 'z' && str2[r] == 'a')) {
+                l++, r++;
+            } else {
+                l++;
+            }
+        }
+        return r == m;
+    }
+};
