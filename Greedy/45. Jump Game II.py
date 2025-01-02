d5f930ff7e35adf2bc74fa524c0b348188123ec0
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-01-02 12:50:33 +0600

    Runtime 3 ms Beats 97.54%

--- Greedy/45. Jump Game II.py
+++ Greedy/45. Jump Game II.py
@@ -14,3 +14,21 @@ class Solution:
             r = farthest
             res += 1
         return res
+    
+
+Function jump(nums: List of Integers) -> Integer:
+    Initialize res to 0  // Number of jumps
+    Initialize l to 0    // Left pointer
+    Initialize r to 0    // Right pointer
+    
+    While r < length(nums) - 1:
+        Initialize farthest to 0  // Farthest point that can be reached
+        
+        For i from l to r:
+            Update farthest to max(farthest, i + nums[i])
+        
+        Set l to r + 1  // Move left pointer to the next range
+        Set r to farthest  // Move right pointer to the farthest point reached
+        Increment res by 1  // Increment number of jumps
+    
+    Return res  // Minimum number of jumps required to reach the last index
