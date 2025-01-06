0bb4127a83857593ec8f185704ab0c413c8d6660
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-01-06 12:14:43 +0600

    Runtime 11 ms | Beats 68.21%

---
+++ Year-2025 Problem List/January 2025/1769. Minimum Number of Operations to Move All Balls to Each Box.py
@@ -0,0 +1,29 @@
+# Problem: 1769. Minimum Number of Operations to Move All Balls to Each Box
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Python Solution
+
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        answer = [0] * n
+
+        balls_to_left = 0
+        moves_to_left = 0
+        balls_to_right = 0
+        moves_to_right = 0
+
+        for i in range(n):
+            # Left pass
+            answer[i] += moves_to_left
+            balls_to_left += int(boxes[i])
+            moves_to_left += balls_to_left
+
+            # Right pass
+            j = n - 1 - i
+            answer[j] += moves_to_right
+            balls_to_right += int(boxes[j])
+            moves_to_right += balls_to_right
+
+        return answer
+
