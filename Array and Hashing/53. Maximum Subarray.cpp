ddbf1f3a0bb92a284f195b9065c8b74544be31dc
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-01-12 19:19:43 +0600

    Runtime: 0ms | Beats: 100.00%

---
+++ Array and Hashing/53. Maximum Subarray.cpp
@@ -0,0 +1,19 @@
+/*
+# Problem          : 53. Maximum Subarray
+# Time Complexity  : O(N)
+# C++ Solution
+*/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int curSum = 0;
+
+        for (int n : nums) {
+            curSum = max(curSum, 0);
+            curSum += n;
+            maxSum = max(maxSum, curSum);
+        }
+        return maxSum;
+    }
+};
