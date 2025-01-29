6d3cc2f24bdea8eda2893899c706d5d5ae0cb4f1
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-01-29 17:23:38 +0600

    Runtime 51 ms | Beats 9.20 %

---
+++ Greedy/215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,21 @@
+/*
+    # Problem          : 215. Kth Largest Element in an Array
+    # Time Complexity  : 
+    # C++ Solution
+*/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for (auto num : nums) {
+            pq.push(num);
+        }
+
+        while (!pq.empty() && k > 1) {
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
