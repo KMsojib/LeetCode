836c80a38c41aa1e1ed4d8ccd4c71534f2f6287c
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-03-09 15:36:45 +0600

    Runtime 766 ms Beats 26.91 % || Memory 21.09 MB Beats 97.34 %

---
+++ Year-2025 Problem List/March 2025/Alternating Groups/3208. Alternating Groups II.py
@@ -0,0 +1,19 @@
+# Problem          : 3208. Alternating Groups II
+# Time Complexity  : O(N)
+# Python Solution
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        n = len(colors)
+        res = 0
+        l = 0
+
+        for r in range(1, n + k - 1):
+            if colors[r % n] == colors[(r - 1) % n]:
+                l = r
+            if r - l + 1 > k:
+                l += 1
+            if r - l + 1 == k:
+                res += 1
+
+        return res
