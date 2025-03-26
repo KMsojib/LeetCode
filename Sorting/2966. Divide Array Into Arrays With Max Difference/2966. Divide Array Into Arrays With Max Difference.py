0bc20fac5cdb601b222107e86c57150409f0aa27
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-03-26 23:36:06 +0600

    Runtime 82 ms Beats 60.97 % || Memory 33.40 MB Beats 48.52 %

---
+++ Sorting/2966. Divide Array Into Arrays With Max Difference/2966. Divide Array Into Arrays With Max Difference.py
@@ -0,0 +1,13 @@
+# Problem          : 2966. Divide Array Into Arrays With Max Difference
+# Time Complexity  : O(n log n)
+# Python Solution
+
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(0, len(nums), 3):
+            if nums[i + 2] - nums[i] > k:
+                return []
+            ans.append([nums[i], nums[i + 1], nums[i + 2]])
+        return ans
