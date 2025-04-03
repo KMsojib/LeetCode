5d33372f3ed0bbd394a0a9a5ead4c7555d204dad
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-04-03 11:27:24 +0600

    Runtime 2 ms Beats 84.98 % || Memory 86.00 MB Beats 24.18 %

---
+++ Array and Hashing/Minimum Sum of Mountain Triplets/2909. Minimum Sum of Mountain Triplets II.cpp
@@ -0,0 +1,28 @@
+# Problem          : 2909. Minimum Sum of Mountain Triplets II
+# Time Complexity  : O(N)
+# C++ Solution
+
+class Solution {
+public:
+    int minimumSum(vector<int>& nums) {
+        int n = (int)nums.size();
+        int minSum = INT_MAX;
+
+        vector<int> prefixMin(n), suffixMin(n);
+        prefixMin[0] = nums[0];
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            prefixMin[i] = min(prefixMin[i - 1], nums[i]);
+            suffixMin[n - 1 - i] = min(suffixMin[n - i], nums[n - 1 - i]);
+        }
+
+        for (int j = 1; j < n - 1; j++) {
+            if (prefixMin[j - 1] < nums[j] && nums[j] > suffixMin[j + 1]) {
+                minSum =
+                    min(minSum, prefixMin[j - 1] + nums[j] + suffixMin[j + 1]);
+            }
+        }
+        return (minSum == INT_MAX) ? -1 : minSum;
+    }
+};
