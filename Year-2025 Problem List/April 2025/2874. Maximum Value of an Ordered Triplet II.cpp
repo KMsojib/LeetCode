9eb2bfb66b59437ece57a875ef36dc1f2ac0febc
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-04-03 10:22:28 +0600

    Runtime 8 ms Beats 38.72 % || Memory 98.72 MB Beats 42.06 %

---
+++ Year-2025 Problem List/April 2025/2874. Maximum Value of an Ordered Triplet II.cpp
@@ -0,0 +1,21 @@
+# Problem          : 2874. Maximum Value of an Ordered Triplet II
+# Time Complexity  : O(N)
+# Space Complexity : O(N)
+# C++ Solution
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> leftMax(n), rightMax(n);
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], nums[i - 1]);
+            rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - i]);
+        }
+        long long res = 0;
+        for (int j = 1; j < n - 1; j++) {
+            res = max(res, (long long)(leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return res;
+    }
+};
