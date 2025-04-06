4575798c7d6a8b3b16ab7308f88e15956b1fcfbe
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-04-06 19:27:05 +0600

    Runtime 1982 ms Beats 6.14 % || Memory 152.14 MB Beats 5.08 %

---
+++ Year-2025 Problem List/April 2025/368. Largest Divisible Subset.py
@@ -0,0 +1,24 @@
+# Problem          : 368. Largest Divisible Subset
+# C++ / Python Solution
+
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        cache = {}
+
+        def dfs(i, prev):
+            if i == len(nums):
+                return []
+            if (i, prev) in cache:
+                return cache[(i, prev)]
+
+            res = dfs(i + 1, prev)
+            if nums[i] % prev == 0:
+                tmp = [nums[i]] + dfs(i + 1, nums[i])
+                res = tmp if len(tmp) > len(res) else res
+
+            cache[(i, prev)] = res
+            return res
+
+        return dfs(0, 1)
