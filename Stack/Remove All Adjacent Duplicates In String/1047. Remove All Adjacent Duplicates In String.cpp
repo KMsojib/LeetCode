735b95a53dd489b48c3283e6620b899b3c56c174
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-04-19 09:02:24 +0600

    Runtime: 19ms, Beats 83.04% || Memory: 18.76MB, Beats 53.75%

--- Stack/Remove All Adjacent Duplicates In String/1047. Remove All Adjacent Duplicates In String.cpp
+++ Stack/Remove All Adjacent Duplicates In String/1047. Remove All Adjacent Duplicates In String.cpp
@@ -27,3 +27,15 @@ public:
         return ans;
     }
 };
+
+
+======================================================== Python =======================================================
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
