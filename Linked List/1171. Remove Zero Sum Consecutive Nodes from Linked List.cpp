ba1acce71b4f77840e614991392f13538e467a74
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-04-22 20:57:08 +0600

    Runtime 0 ms Beats 100.00 % || Memory 18.04 MB Beats 52.39 %

--- Linked List/1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp
+++ Linked List/1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp
@@ -28,3 +28,27 @@ public:
         return result;
     }
 };
+
+==================================== Python =========================================
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prefix_sum_map = {}
+        prefix_sum = 0
+
+        curr = dummy
+        while curr:
+            prefix_sum += curr.val
+            prefix_sum_map[prefix_sum] = curr
+            curr = curr.next
+
+        prefix_sum = 0
+        curr = dummy
+        while curr:
+            prefix_sum += curr.val
+            curr.next = prefix_sum_map[prefix_sum].next
+            curr = curr.next
+
+        return dummy.next
