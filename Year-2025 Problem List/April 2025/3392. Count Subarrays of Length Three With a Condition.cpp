ca45fdf2b6fe02aecf13d98360c2d660e12f57be
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-04-27 11:10:55 +0600

    Runtime 2 ms Beats 92.65 % || Memory 18.04 MB Beats 7.35 %

--- Year-2025 Problem List/April 2025/3392. Count Subarrays of Length Three With a Condition.cpp
+++ Year-2025 Problem List/April 2025/3392. Count Subarrays of Length Three With a Condition.cpp
@@ -17,3 +17,13 @@ public:
         return cnt;
     }
 };
+
+================================== Python ========================================
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        cnt = 0
+        for i in range(1, n - 1):
+            if nums[i] == (nums[i - 1] + nums[i + 1]) * 2:
+                cnt += 1
+        return cnt
