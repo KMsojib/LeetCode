0334aab33e3a67fa42f6a32319e74d9bb7053506
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-05-20 21:00:56 +0600

    Runtime 0 ms Beats 100.00 % || Memory 16.83 MB Beats 9.63 %

---
+++ Array and Hashing/1991. Find the Middle Index in Array.cpp
@@ -0,0 +1,27 @@
+# Problem          : 1991. Find the Middle Index in Array
+# Time Complexity  : O(n)
+# C++ / Python Solution
+
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n = (int)nums.size();
+        vector<int> leftPref(n, 0), rightSuf(n, 0);
+        leftPref[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftPref[i] = leftPref[i - 1] + nums[i];
+        }
+
+        rightSuf[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightSuf[i] = rightSuf[i + 1] + nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (leftPref[i] == rightSuf[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
