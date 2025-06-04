2523d626b2a795e5495db8fbde17ac217c55a4b5
Md Kawsar Mahmud <kawsarmahmud822@gmail.com>
2025-06-04 12:56:43 +0600

    Runtime 72 ms Beats 48.51 % || Memory 169.30 MB Beats 24.63 %

---
+++ Year-2025 Problem List/June 2025/3403. Find the Lexicographically Largest String From the Box I.cpp
@@ -0,0 +1,18 @@
+# Problem          : 3403. Find the Lexicographically Largest String From the Box I
+# Time Complexity  : O(n^2)
+# C++ / Python Solution
+
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        int n = word.size();
+        string res;
+        for (int i = 0; i < n; i++) {
+            res = max(res, word.substr(i, min(n - numFriends + 1, n - i)));
+        }
+        return res;
+    }
+};
