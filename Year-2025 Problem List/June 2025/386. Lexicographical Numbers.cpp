58bc06846671a8f57b8126a72d09164bd11c70c3
KMsojib <kawsarmahmud822@gmail.com>
2025-06-08 07:02:39 -0700

    Aila Jadu

--- Year-2025 Problem List/June 2025/386. Lexicographical Numbers.cpp
+++ Year-2025 Problem List/June 2025/386. Lexicographical Numbers.cpp
@@ -2,3 +2,26 @@
 # Time Complexity  : O(n)
 # space Complexity : O(1)
 # C++ / Python Solution
+
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> result;
+        int current = 1;
+
+        for (int i = 0; i < n; ++i) {
+            result.push_back(current);
+            if (current * 10 <= n) {
+                current *= 10; // Go to the next level
+            } else {
+                while (current % 10 == 9 || current + 1 > n) {
+                    current /= 10; // Backtrack to the parent
+                }
+                current++; // Move to the next sibling
+            }
+        }
+
+        return result;
+    }
+};
