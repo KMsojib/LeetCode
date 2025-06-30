49732ff8cf7217691e08b083006da7828ac64440
KMsojib <kawsarmahmud822@gmail.com>
2025-06-30 11:24:10 +0600

    Succesfully Complete June 2025

---
+++ Year-2025 Problem List/June 2025/594. Longest Harmonious Subsequence.cpp
@@ -0,0 +1,31 @@
+/*
+# Problem          : 594. Longest Harmonious Subsequence
+# Time Complexity  : 
+# space Complexity : 
+# C++ Solution
+*/
+
+/*
+# Approach         : Sliding Window
+# Time Complexity  : O(n log n)
+# Space Complexity : O(1)
+*/
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int n = (int)nums.size();
+        int left = 0, longHarm = 0;
+
+        sort(nums.begin(), nums.end());
+
+        for (int right = 0; right < n; right++) {
+            while (nums[right] - nums[left] > 1) {
+                left++;
+            }
+            if (nums[right] - nums[left] == 1) {
+                longHarm = max(longHarm, right - left + 1);
+            }
+        }
+        return longHarm;
+    }
+};
