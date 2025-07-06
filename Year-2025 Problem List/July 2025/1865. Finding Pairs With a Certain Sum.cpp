8c93809185adb69769402da75b29caaebcada031
KMsojib <kawsarmahmud822@gmail.com>
2025-07-06 19:59:18 +0600

    Runtime 81 ms Beats 76.83 % || Memory 81.83 MB Beats 87.68 %

--- Year-2025 Problem List/July 2025/1865. Finding Pairs With a Certain Sum.cpp
+++ Year-2025 Problem List/July 2025/1865. Finding Pairs With a Certain Sum.cpp
@@ -3,6 +3,40 @@
 # C++ Solution
 */
 
+/* Brute Forces */
+// TLE 
+class FindSumPairs {
+public:
+    vector<int> vp1, vp2;
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        vp1.clear();
+        vp2.clear();
+        vp1 = nums1;
+        vp2 = nums2;
+    }
+
+    void add(int index, int val) { vp2[index] += val; }
+
+    int count(int tot) {
+        int cnt = 0;
+        for (int i = 0; i < vp1.size(); i++) {
+            for (int j = 0; j < vp2.size(); j++) {
+                if (vp1[i] + vp2[j] == tot) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
+
 class FindSumPairs {
 private:
     vector<int> nums1, nums2;
