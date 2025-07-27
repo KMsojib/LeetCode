b39ff63b05c80a9ac26b3d34fab398ffd565d4cc
KMsojib <kawsarmahmud822@gmail.com>
2025-07-27 23:17:17 +0600

    Runtime 11 ms Beats 27.55 % || Memory 14.18 MB Beats 80.16 %

---
+++ Array and Hashing/1365. How Many Numbers Are Smaller Than the Current Number.cpp
@@ -0,0 +1,27 @@
+/*
+# Problem          : 1365. How Many Numbers Are Smaller Than the Current Number
+# Time Complexity  : 
+# space Complexity : 
+# C++ Solution
+*/
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> res;
+
+        int n = (int)nums.size();
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    if (nums[i] > nums[j]) {
+                        cnt++;
+                    }
+                }
+            }
+            res.push_back(cnt);
+        }
+        return res;
+    }
+};
