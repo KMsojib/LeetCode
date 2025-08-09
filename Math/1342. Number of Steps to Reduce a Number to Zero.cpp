39bfe2fa9b42752c7a9d2d3cc2af9007db34244b
KMsojib <kawsarmahmud822@gmail.com>
2025-08-09 10:10:52 +0600

    Runtime 0 ms Beats 100.00 % || Memory 7.87 MB Beats 79.03 %

---
+++ Math/1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,35 @@
+/*
+# Problem          : 1342. Number of Steps to Reduce a Number to Zero
+# Time Complexity  : 
+# space Complexity : 
+# C++ Solution
+*/
+
+// Aproach :- 1
+
+class Solution {
+public:
+    int numberOfSteps(int n) {
+        int cnt = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                cnt++;
+                n /= 2;
+            } else {
+                n -= 1;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+// Runtime 0 ms Beats 100.00 % || Memory 7.88 MB Beats 79.03 %
+
+
+// Aproach :- 2 Binary count
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        return num ? 31 - __builtin_clz(num) + __builtin_popcount(num) : 0;
+    }
+};
