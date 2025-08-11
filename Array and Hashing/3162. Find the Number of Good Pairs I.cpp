d6fbe6f8e1b2637dd624596c57d5dfa2e75d300d
KMsojib <kawsarmahmud822@gmail.com>
2025-08-11 22:34:15 +0600

    Runtime 0 ms Beats 100.00 % || Memory 42.42 MB Beats 9.22 %

---
+++ Array and Hashing/3162. Find the Number of Good Pairs I.cpp
@@ -0,0 +1,50 @@
+/*
+# Problem          : 
+# Time Complexity  : 
+# space Complexity : 
+# C++ Solution
+*/
+
+
+/*
+Brute Force Solution
+Runtime 0 ms Beats 100.00 % || Memory 42.21 MB Beats 79.92 %
+*/
+// class Solution {
+// public:
+//     int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+//         int cnt = 0, n = (int)nums1.size(), m = (int)nums2.size();
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (nums1[i] % (nums2[j] * k) == 0) {
+//                     cnt += 1;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int cnt = 0, n = (int)nums1.size(), m = (int)nums2.size();
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (nums2[j] > nums1[i]) {
+                    break;
+                } else {
+                    if (nums1[i] % (nums2[j] * k) == 0) {
+                        cnt += 1;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
